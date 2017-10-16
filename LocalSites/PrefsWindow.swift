7e9b9f3384e9b0acf783bf6f366d7599a3d6b026
Magnus Wissler <magwiz@icloud.com>
2017-10-16 16:38:59 +0200

    WIP: Add browsing domains

--- LocalSites/PrefsWindow.swift
+++ LocalSites/PrefsWindow.swift
@@ -12,10 +12,12 @@ protocol PrefsWindowDelegate {
   func prefsDidUpdate()
 }
 
-class PrefsWindow: NSWindowController, NSWindowDelegate {
+class PrefsWindow: NSWindowController, NSWindowDelegate, NSTableViewDataSource {
 
   @IBOutlet weak var monochromeIconCheckbox: NSButton!
-
+  @IBOutlet weak var browseDomainsTable: NSTableView!
+    
+  var browseDomains = [ "wittyname.net.", "dns-sd.org."]
   var delegate: PrefsWindowDelegate?
 
   override var windowNibName : NSNib.Name! {
@@ -29,14 +31,32 @@ class PrefsWindow: NSWindowController, NSWindowDelegate {
     self.window?.makeKeyAndOrderFront(nil)
     NSApp.activate(ignoringOtherApps: true)
 
+    
     let defaults = UserDefaults.standard
     let monochrome = defaults.bool(forKey: "monochromeIcon")
     monochromeIconCheckbox.state = monochrome ? NSControl.StateValue.on : NSControl.StateValue.off
+    
+    if let domainList = defaults.array(forKey: "browseDomains") as? [String] {
+        browseDomains = domainList
+    }
+    browseDomainsTable.dataSource = self
   }
 
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return browseDomains.count
+    }
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        
+        let view = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "domainCell"), owner: nil) as! NSTextFieldCell
+        view.stringValue = browseDomains[row]
+        return view
+    }
+    
   func windowWillClose(_ notification: Notification) {
     let defaults = UserDefaults.standard
     defaults.setValue(monochromeIconCheckbox.state==NSControl.StateValue.on, forKey: "monochromeIcon")
+    defaults.setValue(browseDomains, forKey: "browseDomains")
     delegate?.prefsDidUpdate()
   }
 
